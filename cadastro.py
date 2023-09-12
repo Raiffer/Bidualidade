02d756967896fa1706f30f6e092c48853ae1fb81
Raiffer <bernardoraiffer@gmail.com>
2023-09-12 18:59:50 -0300

    Criei Cadastro
    novo cadastro funcional feito com chat GPT

---
+++ cadastro.py
@@ -0,0 +1,37 @@
+# Cria um dicionário vazio para armazenar os dados de cadastro
+cadastros = {}
+
+# Função para registrar um novo usuário
+def registrar_usuario():
+    nome = input("Digite seu nome: ")
+    email = input("Digite seu e-mail: ")
+    senha = input("Digite sua senha: ")
+    cadastros[email] = {'Nome': nome, 'Senha': senha}
+    print("Cadastro realizado com sucesso!")
+
+# Função para exibir todos os usuários registrados
+def listar_usuarios():
+    print("\nLista de Usuários Registrados:")
+    for email, dados in cadastros.items():
+        print(f"Email: {email}")
+        print(f"Nome: {dados['Nome']}")
+        print(f"Senha: {dados['Senha']}\n")
+
+# Menu principal
+while True:
+    print("\nEscolha uma opção:")
+    print("1. Registrar novo usuário")
+    print("2. Listar usuários registrados")
+    print("3. Sair")
+
+    opcao = input("Digite o número da opção desejada: ")
+
+    if opcao == '1':
+        registrar_usuario()
+    elif opcao == '2':
+        listar_usuarios()
+    elif opcao == '3':
+        print("Saindo do programa. Até logo!")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
